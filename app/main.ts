690111523a5cdc6da887fbaf2e1ba96ef1ff5575
kanocarra <emily.melhuish@gmail.com>
2016-07-25 09:35:09 +1200

    Two way binding on hero name working

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by emelhuish on 25/07/2016.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
